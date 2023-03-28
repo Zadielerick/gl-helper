e206c4c1574704433010fadc832fa30a740926f6
Erick Narvaez <zadielerick@gmail.com>
2023-03-27 20:27:46 -0500

    gl: Adds Shader class
    The Shader class takes care of compiling and linking the shaders.
    Currently only supports vertex and fragment shaders.
    
    The preferred usage is to create a ShaderProgram object and initialize
    it with two strings containing the vertex and fragment shader code.
    Then, call the compileShaderProgram() member function to generate the
    program.
    
    Signed-off-by: Erick Narvaez <zadielerick@gmail.com>

---
+++ include/Shader.h
@@ -0,0 +1,58 @@
+#ifndef _SHADER_H_
+#define _SHADER_H_
+
+#include <vector>
+#include <string>
+
+#include <GLES2/gl2.h>
+
+/* Shader Class
+ * Handles the following tasks for OpenGL:
+ * - GLSL shader compilation and error reporting
+ * - OpenGL shader program
+ */
+class Shader
+{
+protected:
+	GLenum shaderType;
+	GLuint shader;
+	std::string source;
+
+public:
+	Shader(GLenum shaderType);
+	Shader(GLenum shaderType, std::string source);
+	GLenum getShaderType() { return shaderType; }
+	GLuint getShader() { return shader; }
+	void compileShader();
+};
+
+/* Vertex Shader Class */
+class VertexShader : public Shader
+{
+public:
+	VertexShader();
+	VertexShader(std::string source);
+};
+
+/* Fragment Shader Class */
+class FragmentShader : public Shader
+{
+public:
+	FragmentShader();
+	FragmentShader(std::string source);
+};
+
+/* Shader Program Class
+ * Composition of a Vertex Shader and Fragment Shader for compiling the linked program
+ */
+class ShaderProgram
+{
+	VertexShader vs;
+	FragmentShader fs;
+	GLuint program;
+public:
+	ShaderProgram(std::string vsSource, std::string fsSource);
+	void compileShaderProgram();
+	GLuint getProgram() { return program; }
+};
+#endif /* _SHADER_H_ */
